6da0cab11439afa8500f96344eb19e4fd09a9410
JaredBeh <jaredbeh@outlook.com>
2016-03-06 22:31:49 -0500

    stupid windows error

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-edges = new_matrix(4,2)
-for x in range(500):
-    for y in range(500):
+edges = new_matrix()
+master = ident(new_matrix())
+edges = add_edge(edges,0,0,0,20,10,0)
+for n in range(200):
+    if(n%2 == 0):
+        master = make_translate(10,10,0)
+    elif(n%3 == 0):
+        master = make_rotZ(142)
+    else:
+        master = make_scale(2,2,1)
+    draw_lines(edges,screen,color)
+    master = ident(new_matrix())
 
 display(screen)
